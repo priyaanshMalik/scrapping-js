d6309d087b29d7b28f2ddc97f4b20862accc5b40
priyaansh <priyaanshmalik@outlook.com>
2021-09-09 18:04:04 +0530

    improved

--- scrap1.js
+++ scrap1.js
@@ -1,16 +1,27 @@
+// |________________________________________________________________________________________________|
+// |About:--                                                                                        |
+// |this is the program to extract last ball delivery commentonry from espn cricinfo website        |
+// |this program also extracts all bolwers of the match and their respective wickets and            |
+// |        calculates the highest wicket taker                                                     |
+// |the program uses the module 'request' to request data from the webpage and then makes use of    |
+// |        the cheerio module for data extraction from the requested webpage                       |
+// |________________________________________________________________________________________________|
+
 let request=require("request")  //importing request.
 let cheerio=require("cheerio")  // importing cheerio.
 let fs=require("fs")  // importing fs module
 
 var url1='https://www.espncricinfo.com/series/2020-21-12510595/royal-chalengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/full-scorecard'
 var url2='https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/ball-by-ball-commentary'
-// url of webpages
+// url of webpages↑
 
-// request(url,cb); //accessing webpage using a callback function
+// request(url,cb); //accessing webpage using a callback function syntax
 request(url1,cb1); //accessing webpage1 using a callback function
 request(url2,cb2) //accessing webpage2 using a callback function
 
-function cb1(error,response, html) { //a callback function
+
+
+function cb1(error,response, html) { //a callback function for url1
     if (error){
         console.log("error", error);    //for error
     }
@@ -20,64 +31,65 @@ function cb1(error,response, html) { //a callback function
     else{
         // console.log("response", response);
         // console.log(html)   //prints html file of requested page
-        // console.log("body", body);
-        console.log('\t\t\tfetching')
+        console.log('\t\t  fetching wickets data')
         console.log('...............................................')
-        dataExtractor1(html)
-        // function for extracting data
+        dataExtractor1(html)    // function for extracting data
     }
 }
-function cb2(error,response, html) { //a callback function
+function cb2(error,response, html) { //a callback function for url2
     if (error){
         console.log("error", error);    //for error
     }
     else if(response.statusCode==404){  
-        console.log("Err! Page not found")
+        console.log("Err! Page not found");
     }
     else{
-        console.log('\t\t\tfetching')
+        console.log('\t\t  fetching commentory data')
         console.log('...............................................')
-        dataExtractor2(html)
+        dataExtractor2(html);
     }
 }
-function dataExtractor1(html){  //search tool
+function dataExtractor1(html){  //search tool for hoghest wicket taker.
     let searchTool=cheerio.load(html); //global tool
     let bowlers=searchTool(".table.bowler tbody tr");
-    // for(let i=0; i<bowlerTables.length;i++){
-    //   let player=searchTool(bowlerTables[i]).find("tr")
-    // }
-    console.log('bowlers:')
-    let bowler="";
+    console.log('bowlers:\t\twickets');
+    let theBowler="";
     let highestWickets=0;
-    for(let i=0;i<bowlers.length;i++){
-      let cols=searchTool(bowlers[i]).find('td');
-      let bowlerName=searchTool(cols[0]).text();    //whenever index is used searTool is used again.
-      let bowlerWickets=searchTool(cols[4]).text()
-      
-      console.log(bowlerName+"\t"+bowlerWickets)
+    for(let i=0;i<bowlers.length;i++){  //iterating through each bowler
+      let cols=searchTool(bowlers[i]).find('td');   //each bowler
+      //whenever index is used searTool is used again.
+      let bowlerName=searchTool(cols[0]).text().trim();    //for name of bowler
+      let bowlerWickets=searchTool(cols[4]).text().trim(); //for number of wickets
+      if(bowlerName!=''){ //to check for empty bowlerName
+            if(bowlerName.length <16)   //for formatting text output
+                console.log(bowlerName+"\t\t"+bowlerWickets);   //wickets for each bowler.
+            else
+                console.log(bowlerName+"\t"+bowlerWickets);   //wickets for each bowler.  
+      }
+
       if(bowlerWickets > highestWickets){
-        bowler=bowlerName
-        highestWickets=bowlerWickets
+        theBowler=bowlerName;
+        highestWickets=bowlerWickets;   //hoghest wicket taker till now
       }
       else if(bowlerWickets == highestWickets){
-        bowler=bowler + ", " + bowlerName
+        theBowler=theBowler + ", " + bowlerName;  //for multiple highest wicket takers
       }
     }
     console.log('__________________________________________________')
     console.log('\thighest wicket taker:')
-    console.log("name: "+bowler+',\twickets: ', + highestWickets)
+    console.log("name: "+theBowler+',\twickets: ', + highestWickets)
 }
-
-function dataExtractor2(html){
+function dataExtractor2(html){ //search tool for last ball commentry
     let searchTool = cheerio.load(html);
+    let element = searchTool(".match-comment-wrapper .match-comment-long-text");
     // you always can't get unique selectors
-    let elemRepArr = searchTool(".match-comment-wrapper .match-comment-long-text");
-    // console.log(elemRepArr.length);
-    let lbc = searchTool(elemRepArr[0]).text();
+    // console.log(element.length); //gets all commentry of the webpage
+    let lastBallCommentory = searchTool(element[0]).text(); //first index contains last ball commentry
     console.log('__________________________________________________')
-    console.log('\tlast ball commentory')
-    console.log("~", lbc);
+    console.log('\tlast ball commentory:-')
+    console.log("~", lastBallCommentory);
+    console.log('__________________________________________________')
 }
 
 console.log("\t\twaiting for response\t");
-console.log("...............................................")
+console.log("...................................................")
--- scrap2.js
+++ scrap2.js
@@ -1,17 +1,25 @@
+// |________________________________________________________________________________________________|
+// |About:--                                                                                        |
+// |the following program makes a request ot espncricinfo webpage and retrieves the birthdays for   |
+// |    bowlers of a particular match.                                                              |
+// |This is done by requesting a wepage for each bowler each time and extracting birthday           |
+// |        information                                                                             |
+// |________________________________________________________________________________________________|
+
 let request=require("request")  //importing request.
 let cheerio=require("cheerio")  // importing cheerio.
 let fs=require("fs")  // importing fs module
 
-var url1='https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/full-scorecard'
+var url='https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/full-scorecard'
 // url of webpages
 
-request(url1,cb); //accessing webpage1 using a callback function
+request(url,cb); //accessing webpage1 using a callback function
 
-function cb(error,response, html) { //a callback function
+function cb(error,response, html) { //a callback function for request
     if (error){
-        console.log("error", error);    //for error
+        console.log("error", error);
     }
-    else if(response.statusCode==404){  //error 404 also gives back a webpage
+    else if(response.statusCode==404){  
         console.log("Err! Page not found")
     }
     else{
@@ -20,23 +28,25 @@ function cb(error,response, html) { //a callback function
         dataExtractor(html) // function for extracting data
     }
 }
-// let rootlink='https://www.espncricinfo.com'
+// rootlink is 'https://www.espncricinfo.com'
 function dataExtractor(html){  //search tool
     let searchTool = cheerio.load(html);
     let bowlers = searchTool(".table.bowler tbody tr");
     console.log('key:\ty-years, d-days\n')
     for (let i = 0; i < bowlers.length; i++) {
         let cols = searchTool(bowlers[i]).find("td");
-        let anchorElem = searchTool(cols[0]).find("a")
-        let link = anchorElem.attr("href");
-        let fullLink = `https://www.espncricinfo.com${link}`;
-        // console.log(fullLink)
-        request(fullLink, newcb); //requesting data from new new webpage i.e. from url==fullLink
+        let anchorElement = searchTool(cols[0]).find("a")   //find link through <a> element
+        let link = anchorElement.attr("href");  //extracting link through href attribute of <a>
+        // console.log(link)
+        if(link!=undefined){    //if link obtained isb't undefined ...
+            let fullLink = `https://www.espncricinfo.com${link}`;   //formig full path as only relative path is extracted
+            request(fullLink, newcb); //requesting data from new new webpage i.e. from url==fullLink
+        }
     }
 }
-function newcb(error, response, html) {
+function newcb(error, response, html) { //callback function for request of each webpage for each bowler
     if (error) {
-        console.log(error); // Print the error
+        console.log('an error occured while requesting page. could not retrieve information'); // Print the error
     } else if (response.statusCode == 404) {
         console.log("Page Not Found")
     }
